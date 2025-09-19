e637f447fe6b53abca1bab1be73fbd2805832144
Ahmad Nazar <ahmadfaraznazar@gmail.com>
2025-09-20 01:43:01 +0500

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,12 @@ import Orders from './pages/Orders';
 import PlaceOrder from './pages/PlaceOrder';
 import Product from './pages/Product';
 import About from './pages/About';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
@@ -20,8 +22,8 @@ const App = () => {
         <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login />} />
         <Route path="/orders" element={<Orders />} />
-        <Route path="/placeOrder" element={<PlaceOrder />} />
-        <Route path="/product" element={<Product />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/product/:productId" element={<Product />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </div>
