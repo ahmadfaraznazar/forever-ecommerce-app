848f0cf2f9b1d15a965914c8ba591155a926ac60
Ahmad Nazar <ahmadfaraznazar@gmail.com>
2025-10-15 01:35:43 +0500

    fix api route slashes

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -51,7 +51,7 @@ const ShopContextProvider = (props) => {
 
     if (token) {
       try {
-        await axios.post(backendUrl + '/api/cart/add', { itemId, size }, { headers: { token } });
+        await axios.post(backendUrl + 'api/cart/add', { itemId, size }, { headers: { token } });
       } catch (error) {
         console.log(error);
         toast.error(error.message);
@@ -86,7 +86,7 @@ const ShopContextProvider = (props) => {
 
     if (token) {
       try {
-        await axios.post(backendUrl + '/api/cart/update', { itemId, size, quantity }, { headers: { token } });
+        await axios.post(backendUrl + 'api/cart/update', { itemId, size, quantity }, { headers: { token } });
       } catch (error) {
         console.log(error);
         toast.error(error.message);
@@ -132,7 +132,7 @@ const ShopContextProvider = (props) => {
   // Fetch user cart data from the backend
   const getUserCart = async (token) => {
     try {
-      const response = await axios.post(backendUrl + '/api/cart/get', {}, { headers: { token } });
+      const response = await axios.post(backendUrl + 'api/cart/get', {}, { headers: { token } });
       if (response.data.success) {
         setCartItems(response.data.cartData);
       }
